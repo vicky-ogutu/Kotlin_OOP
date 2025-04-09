9e35b7f044f11d378a4df9d0dd93c6d4804b3d39
vicky-ogutu <vikogutu77@gmail.com>
2025-04-09 21:31:18 +0300

    constructor_methods

--- src/main/kotlin/Constructor.kt
+++ src/main/kotlin/Constructor.kt
@@ -1,6 +1,16 @@
-class Constructor (var model:String, var price:Int)
+class Constructor (var model:String, var price:Int){
+    fun drive(){
+        println("no noise")
+    }
+
+    fun driver(name:String){
+        print(name)
+    }
+}
 
 fun main(){
     var obj1 =Constructor("Subaru", 1300000)
     println(obj1.model + " " + obj1.price)
+    obj1.drive()
+    obj1.driver("Omwami")
 }
