9334297daece522b6a3655d06a397245eb100096
vicky-ogutu <vikogutu77@gmail.com>
2025-04-09 20:52:54 +0300

    fun_with_return_type

--- src/main/kotlin/Function_with_parameters.kt
+++ src/main/kotlin/Function_with_parameters.kt
@@ -4,8 +4,14 @@
 
 fun main(){
    names("Oguttu")
+  var result =  add_two_nums(10,20)
+    print(result)
 }
 
 fun names(lname:String){
     println("Vicky" +" " +lname)
+}
+
+fun add_two_nums(x:Int, y:Int):Int{
+    return x+y;
 }
