e69cd57421ea51be6ab63fe91a640e57224d18f9
vicky-ogutu <vikogutu77@gmail.com>
2025-04-09 21:16:50 +0300

    car_object_properties

---
+++ src/main/kotlin/Car.kt
@@ -0,0 +1,19 @@
+
+
+class Car {
+    var make =""
+    var color =""
+    var price =0
+
+}
+
+fun main(){
+    var obj1 =Car()
+    obj1.make ="Subaru"
+    obj1.color="White"
+    obj1.price= 1200000
+
+    println(obj1.make)
+    println(obj1.color)
+    println(obj1.price)
+}
